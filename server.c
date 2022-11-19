4dd884f0e8367b0e79d08f4c1a727b2a39e0c0a3
seanchung2001 <seanchung@cmail.carleton.ca>
2022-11-19 09:48:32 -0500

    Added server.c file for test

---
+++ server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/neutrino.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+#include "server.h"
+
+
+int main(void) {
+	//Add code (Mohammad's section)
+
+		//Add code (sean's section)
+	}
+
+	return 0;
+}
