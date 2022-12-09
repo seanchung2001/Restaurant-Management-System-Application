1582156a68ba4962ed5110b82422e9ccd148daca
Sean Chung <72354991+seanchung2001@users.noreply.github.com>
2022-12-09 08:53:33 -0500

    Update server.c
    Added comments to code Sean

--- server.c
+++ server.c
@@ -19,6 +19,7 @@ typedef union
 	recv_table_print_receipt_t print_receipt;
 } recv_buf_t;
 
+//For casting void * to type int and long long
 typedef union
 {
 	void *ptr;
@@ -41,7 +42,7 @@ int insert_tables_database(qdb_hdl_t *hdl);
 
 //Database structs
 table_t tables[MAX_NUM_TABLES];\
-//For debugging, tables will be read in from database
+
 int tables_size = 0;
 
 menu_item_t menu[MAX_MENU_ITEMS];
@@ -90,12 +91,17 @@ int main(void) {
 	struct sigevent sigevent;
 	struct itimerspec itime;
 	timer_t timerID;
+	
 	int coid;
+	
+	//flags for checking return value of functions
 	int get_table_num;
 	int create_table_order_flag;
 	int create_reservation_flag;
 	float online_order_flag;
 	float printReceiptTotal;
+	
+	//unique order ID's for table, online and reservation order
 	int tableOrderID = 1000;
 	int reservationID = 2000;
 	int onlineOrderID = 3000;
@@ -339,7 +345,7 @@ int main(void) {
 						num_online_orders_future = 0;
 						num_table_receipts = 0;
 
-						//Get reservations for current day
+						//Get reservations for current day from database
 						get_reservation(hdl, &getReservations, &reservationNumRows, &reservationNumCols, calculate_current_year(dateTime.tm_year), calculate_current_month(dateTime.tm_mon), dateTime.tm_mday);
 						for (int i = 0; i < reservationNumRows; i++) {
 							reservations_t tmpRes;
@@ -366,9 +372,6 @@ int main(void) {
 							today_reservations[i] = tmpRes;
 							num_today_reservations++;
 						}
-						for (int i = 0; i < num_today_reservations; i++) {
-							printf("reservations[i] for testing, res first name: %s, phone num: %lld, table_num: %d\n", today_reservations[i].first_name, today_reservations[i].phone_num, today_reservations[i].table_num);
-						}
 					break;
 				}
 			}
@@ -381,6 +384,7 @@ int main(void) {
 				case CREATE_RESERVATION_MSG_TYPE:
 					create_reservation_flag = create_reservation(client_msg.reservation.num_people, client_msg.reservation.table_meta_tag, client_msg.reservation.year, client_msg.reservation.month, client_msg.reservation.day, client_msg.reservation.first_name, client_msg.reservation.last_name, client_msg.reservation.phone_num, client_msg.reservation.start_hour, client_msg.reservation.start_min, client_msg.reservation.end_hour, client_msg.reservation.end_min, calculate_current_year(dateTime.tm_year), calculate_current_month(dateTime.tm_mon), dateTime.tm_mday, reservationID);
 					resp_msg_reservation_t resp_reservation_to_client;
+					//If reservation successful, build the reply message to send to client
 					if (create_reservation_flag > 0) {
 						resp_msg_reservation_t resp_reservation_to_client;
 						resp_reservation_to_client.reservationID = reservationID;
@@ -390,7 +394,7 @@ int main(void) {
 						printf("reservation successful\n");
 					}
 					else {
-						//If an error occured, return -1
+						//If an error occured, return -1 to client
 						resp_reservation_to_client.reservationID = -1;
 						resp_reservation_to_client.table_num = -1;
 						MsgReply(rcvid, 0, &resp_reservation_to_client, sizeof(resp_reservation_to_client));
@@ -464,6 +468,7 @@ int main(void) {
 							}
 						}
 					}
+					//If the table order failed, reset all of the menu counts
 					if (create_table_order_flag == -1) {
 						for (int i = 0; i < MAX_MENU_ITEMS; i++) {
 							if (menu[i].count > 0) {
@@ -473,26 +478,32 @@ int main(void) {
 						break;
 					}
 					create_table_order_flag = create_table_order(client_msg.table_order.table_num, tableOrderID);
+					//Everything worked, success!
 					if (create_table_order_flag == 0) {
 						//This will send the total amount of the order as a message back to the client
 						MsgReply(rcvid, 0, &tableOrderID, sizeof(tableOrderID));
 						tableOrderID++;
 						printf("table order successful\n");
 					}
+					//Something went wrong when creating table order item structs
 					else {
 						MsgReply(rcvid, 0, &create_table_order_flag, sizeof(create_table_order_flag));
 						printf("table order failed\n");
 					}
 					break;
 				case GET_TABLE_IN_HOUSE_MSG_TYPE:
+					//Maximum number of seats requested > 16
 					if (client_msg.get_table.num_people > MAX_NUM_SEATS) {
 						printf("Too many seats requested... most seats for a table is 16\n");
 					}
+					//Find a table using the number of seats requested from client
 					get_table_num = get_table_in_house(client_msg.get_table.num_people);
+					//Found a table
 					if (get_table_num >= 0) {
 						printf("get table in house successful\n");
 						update_table(hdl, get_table_num, 1);
 					}
+					//Was not able to find a table
 					else{
 						printf("get table in house failed\n");
 					}
@@ -502,6 +513,7 @@ int main(void) {
 					printReceiptTotal = table_print_receipt(client_msg.print_receipt.table_num);
 					resp_table_print_receipt_t print_receipt_response;
 					table_order_receipt_t tmpTable_receipt;
+					//There exists table orders for the specified table number
 					if (printReceiptTotal > 0) {
 						//Get the order ID in here and clear up any orders that are completed
 						for (int i = 0; i < num_existing_orders; i++) {
@@ -553,19 +565,23 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 	int cpyNumSeats = num_seats;
 	int found_table = 0;
 	int flag = 0;
+	//Check whether the reservation is for current day
 	if (curYear == year && curMonth == month && curDay == day) {
 		while (cpyNumSeats <= MAX_NUM_SEATS) {
+			//Search for a table
 			for (int i = 0; i < tables_size; i++) {
+				//Found a table
 				if (tables[i].isReserved == 0 && tables[i].num_seats == cpyNumSeats) {
+					//Search for table with the correct table meta-tag (patio, bar, party, etc.)
 					for(int k = 0; k < MAX_TABLE_TAGS; k++) {
 						printf("table num: %d, table num seats: %d\n", tables[i].table_num, tables[i].num_seats);
 						if ((strcmp(table_tags[k].meta_tag_name, meta_tag) == 0 && table_tags[k].table_num == tables[i].table_num) || (strcmp(meta_tag,"") == 0)) {
 							printf("table num: %d, table num seats: %d, table tag: %s\n", tables[i].table_num, tables[i].num_seats, table_tags[k].meta_tag_name);
+							//Check if the table will be reserved during the requested reservation time
 							for (int j = 0; j < num_today_reservations; j++) {
 								if (today_reservations[j].status == 0) {
 									if ((today_reservations[j].table_num == tables[i].table_num && today_reservations[j].start_hour == start_hour) || (today_reservations[j].table_num == tables[i].table_num && today_reservations[j].start_hour == duration_hour) || (today_reservations[j].table_num == tables[i].table_num && today_reservations[j].end_hour == start_hour) || (today_reservations[j].table_num == tables[i].table_num && today_reservations[j].end_hour == duration_hour)) {
 										flag = 1;
-										printf("reservation was here before\n");
 										break;
 									}
 								}
@@ -574,6 +590,7 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 								flag = 0;
 								break;
 							}
+							//Found a table
 							else {
 								found_table = tables[i].table_num;
 								break;
@@ -588,6 +605,7 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 			if (found_table > 0) {
 				break;
 			}
+			//Look for a table with more seats
 			cpyNumSeats++;
 		}
 
@@ -595,6 +613,7 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 			//No tables were found
 			return 0;
 		}
+		//Set all the reservations
 		reservations_t tmpReservation;
 		tmpReservation.day = day;
 		tmpReservation.end_hour = duration_hour;
@@ -615,11 +634,14 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 	else {
 		while (cpyNumSeats <= MAX_NUM_SEATS) {
 			for (int i = 0; i < tables_size; i++) {
+				//Search for a table
 				if (tables[i].num_seats == cpyNumSeats) {
+					//Found a table, search for the table meta tag
 					for(int k = 0; k < MAX_TABLE_TAGS; k++) {
 						//printf("table num: %d, table num seats: %d, table tag: %s\n", tables[i].table_num, tables[i].num_seats, table_tags[k].meta_tag_name);
 						if ((strcmp(table_tags[k].meta_tag_name, meta_tag) == 0 && table_tags[k].table_num == tables[i].table_num) || (strcmp(meta_tag, "") == 0)) {
 							printf("table num: %d, table num seats: %d, table tag: %s\n", tables[i].table_num, tables[i].num_seats, table_tags[k].meta_tag_name);
+							//Check if reservations are made for that table on the specified date and time
 							for (int j = 0; j < num_future_reservations; j++) {
 								if (future_reservations[j].table_num == tables[i].table_num && future_reservations[j].year == year && future_reservations[j].month == month && future_reservations[j].day == day && future_reservations[j].status == 0) {
 									if (future_reservations[j].start_hour == start_hour || future_reservations[j].start_hour == duration_hour || future_reservations[j].end_hour == start_hour || future_reservations[j].end_hour == duration_hour) {
@@ -629,10 +651,12 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 									}
 								}
 							}
+							//Did not find a table, keep searching
 							if (flag == 1) {
 								flag = 0;
 								break;
 							}
+							//Found a table
 							else {
 								found_table = tables[i].table_num;
 								break;
@@ -640,10 +664,12 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 						}
 					}
 				}
+				//Did not find a table, keep searching
 				if (found_table > 0) {
 					break;
 				}
 			}
+			//Did not find a table, keep searching
 			if(found_table > 0) {
 				break;
 			}
@@ -653,6 +679,7 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 			//No tables were found
 			return 0;
 		}
+		//Create reservation struct
 		reservations_t tmpReservation;
 		tmpReservation.day = day;
 		tmpReservation.end_hour = duration_hour;
@@ -669,6 +696,7 @@ int create_reservation(int num_seats, char *meta_tag, int year, int month, int d
 		tmpReservation.id = resID;
 		future_reservations[num_today_reservations++] = tmpReservation;
 	}
+	//Return found table number
 	return found_table;
 }
 
@@ -687,11 +715,14 @@ float create_online_order(int year, int month, int day, int hour, int minute, ch
 			tmpOnlineOrder.orderID = orderID;
 			tmpOnlineOrder.count = 0;
 			printf("menu[i] name: %s, count%d\n", menu[i].name, menu[i].count);
+			//Add the cost to total variable
 			for (int j = 0; j < menu[i].count; j++) {
 				tmpOnlineOrder.count += 1;
 				total += menu[i].price;
 			}
+			//Add order items to array of struct
 			online_order_items[num_online_order_items++] = tmpOnlineOrder;
+			//Reset the menu item count
 			menu[i].count = 0;
 		}
 	}
@@ -710,13 +741,15 @@ float create_online_order(int year, int month, int day, int hour, int minute, ch
 	makeOnlineOrder.year = year;
 	makeOnlineOrder.total = total;
 
+	//Check if online order is for current day
 	if (curYear == year && curMonth == month && curDay == day) {
 		online_orders_today[num_online_orders_today++] = makeOnlineOrder;
 	}
+	//Check if online order is for a future date
 	else {
 		online_orders_future[num_online_orders_future++] = makeOnlineOrder;
 	}
-	//If there were no errors, return total
+	//Print all the online orders, online order items, and menu items
 	for (int i = 0; i < num_online_orders_today; i++) {
 		printf("online order today total: %f\n", online_orders_today[i].total);
 	}
@@ -726,17 +759,15 @@ float create_online_order(int year, int month, int day, int hour, int minute, ch
 	for (int i = 0; i < MAX_MENU_ITEMS; i++) {
 		printf("menu name: %s, count: %d\n", menu[i].name, menu[i].count);
 	}
+	//If there were no errors, return total
 	return total;
 }
 
 int create_table_order(int table_num, int orderID) {
-	//Check if table isReserved
-	/*if (tables[table_num-1].isReserved == 0) {
-		//The table is currently not being used, return error
-		return -1;
-	}*/
+	//Iterate through menu item counts for calculating the total
 	for (int i = 0; i < MAX_MENU_ITEMS; i++) {
 		if (menu[i].count > 0) {
+			//Create table order item structs
 			printf("menu[i]: %s\n", menu[i].name);
 			table_order_item_t tmpTableOrder;
 			strcpy(tmpTableOrder.menu_item_name, menu[i].name);
@@ -744,6 +775,7 @@ int create_table_order(int table_num, int orderID) {
 			tmpTableOrder.order_id = orderID;
 			tmpTableOrder.count = 0;
 			tmpTableOrder.isComplete = 0;
+			//Increment count variable for checking if there are multiple orders of the same menu item
 			for (int j = 0; j < menu[i].count; j++) {
 				tmpTableOrder.count += 1;
 			}
@@ -751,6 +783,7 @@ int create_table_order(int table_num, int orderID) {
 			menu[i].count = 0;
 		}
 	}
+	//Print all the existing table orders (for debugging pruposes)
 	for (int i = 0; i < num_existing_orders; i++) {
 		printf("table_orders_existing table num: %d, menu name: %s, orderID: %d, count: %d\n", table_orders_existing[i].table_num, table_orders_existing[i].menu_item_name, table_orders_existing[i].order_id, table_orders_existing[i].count);
 	}
@@ -761,31 +794,41 @@ int create_table_order(int table_num, int orderID) {
 
 int get_table_in_house(int num_people) {
 	int num_seats = num_people;
-	printf("num_seats: %d\n", num_seats);
 	while (num_seats <= MAX_NUM_SEATS) {
+		//Iterate through the tables
 		for (int i = 0; i < tables_size; i++) {
+			//Check if the number of seats for the table is equal to num_seats. Also check if the table is currently reserved
 			if (tables[i].num_seats == num_seats && tables[i].isReserved == 0) {
+				//If table is found, set the table to reserved and return the table number
 				tables[i].isReserved = 1;
 				return tables[i].table_num;
 			}
 		}
+		//If no tables with specified seat number is found, increment the number of seats and continue searching
 		num_seats++;
 	}
+	//No tables were found, return -1
 	return -1;
 }
 
 float table_print_receipt(int table_num) {
 	float total = 0;
-
+	//Iterate through table order item struct array
 	for (int i = 0; i < num_existing_orders; i++) {
+		//If the table number is found in the table order items
 		if (table_orders_existing[i].table_num == table_num) {
+			//Iterate through all menu items
 			for (int j = 0; j < MAX_MENU_ITEMS; j++) {
+				//compare the menu item name with the menu item name in table order item struct
 				if (strcmp(table_orders_existing[i].menu_item_name, menu[j].name) == 0) {
+					//Multiple orders of the same menu item
 					if (table_orders_existing[i].count > 1) {
+						//Add to the total price
 						for (int k = 0; k < table_orders_existing[i].count; k++) {
 							total += menu[j].price;
 						}
 					}
+					//One order of the menu item
 					else {
 						total += menu[j].price;
 					}
@@ -794,9 +837,11 @@ float table_print_receipt(int table_num) {
 			}
 		}
 	}
+	//If total is 0, no table order items were found with the specified table number, return 0
 	if (total == 0) {
 		return 0;
 	}
+	//If total > 0, return the total
 	return total;
 }
 
@@ -823,14 +868,17 @@ int calculate_current_hour(int hour) {
 
 }
 
+//Calculate current year properly
 int calculate_current_year(int year) {
 	return year + 1900;
 }
 
+//Calculate current month properly
 int calculate_current_month(int month) {
 	return month+1;
 }
 
+//Insert tables into database and table array of structs
 int insert_tables_database(qdb_hdl_t *hdl) {
 	int numSeats = 0;
 	for (int i = 0; i < MAX_NUM_TABLES; i++) {
